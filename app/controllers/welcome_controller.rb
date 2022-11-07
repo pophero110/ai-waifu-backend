9ee49e0c003116b6c730a10b4dfedfdadd94de76
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-11-07 00:45:40 -0600

    add filter and sidebar for mobile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,23 @@
 class WelcomeController < ApplicationController
 	def index
-		@ai_waifus = AiWaifu.includes(:likes, :downloads).order(created_at: :desc).page(params[:page])
+		@ai_waifus = apply_filter.page(params[:page])
+	end
+
+
+	private
+
+	def apply_filter
+		case params[:filtered_by]
+		when "most_likes"
+			AiWaifu.left_joins(:likes).group(:id).order('COUNT(likes.id) DESC')
+		when "least_likes"
+			AiWaifu.left_joins(:likes).group(:id).order('COUNT(likes.id) ASC')
+		when "recent_upload"
+			AiWaifu.includes(:likes, :downloads).order(created_at: :desc)
+		when "old_upload"
+			AiWaifu.includes(:likes, :downloads).order(:created_at)
+		else
+			AiWaifu.includes(:likes, :downloads).order(created_at: :desc)
+		end
 	end
 end
