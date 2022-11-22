278686c258abf98b40a163ccb746e3220b3456a2
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-11-21 18:00:45 -0600

    add jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import '@hotwired/turbo-rails';
 import './controllers';
+import jquery from 'jquery';
 import * as bootstrap from 'bootstrap';
+
+window.$ = jquery;
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 100) {
+        $('.navbar').addClass('semi-transparent');
+    } else {
+        $('.navbar').removeClass('semi-transparent');
+    }
+});
