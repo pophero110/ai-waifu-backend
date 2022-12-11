bf67d4a92c559a1ef1e2988cf784a74282b0d885
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-10 21:36:56 -0600

    refactor user

--- app/models/user.rb
+++ app/models/user.rb
@@ -90,16 +90,6 @@ class User < ApplicationRecord
     !confirmed?
   end
 
-  def send_confirmation_email!
-    confirmation_token = generate_confirmation_token
-    UserMailer.email_confirmation(self, confirmation_token).deliver_now
-  end
-
-  def send_password_reset_email!
-    password_reset_token = generate_password_reset_token
-    UserMailer.password_reset(self, password_reset_token).deliver_now
-  end
-
   def generate_confirmation_token
     signed_id expires_in: CONFIRMATION_TOKEN_EXPIRATION, purpose: :confirm_email
   end
