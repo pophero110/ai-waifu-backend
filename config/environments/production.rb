a6f8d5c2dcd6b0812602fd1f9e9a05ad999f1a3c
pophero110 <38047622+pophero110@users.noreply.github.com>
2022-12-12 22:30:14 -0600

    add domain to smtp setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
+    domain: 'gmail.com',
     user_name: ENV['GACC'],
     password: ENV['GPASS'],
     authentication: 'plain',
